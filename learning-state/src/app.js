4c9cac630b612766168dd22abf3990c12dbbad68
Gabriel de Paula Queiroz <queirozg96@outlook.com>
2020-04-18 11:52:36 -0300

    M1#A27

--- learning-state/src/app.js
+++ learning-state/src/app.js
@@ -1,34 +1,13 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Timer from './timer'
+import Button from './button'
 
 class App extends Component {
-  constructor () {
-    console.log('constructor')
-    super()
-    this.state = {
-      time: 0,
-      showTimer: true
-    }
-  }
-
-  componentWillMount () {
-    console.log('componentWillMount')
-  }
-
-  componentDidMount () {
-    console.log('componentDidMount')
-  }
-
   render () {
-    console.log('render')
     return (
       <div>
-        <Timer time={this.state.time} />
-        <button onClick={() => {
-          this.setState({ time: this.state.time + 10 })
-        }}>Change props</button>
+        <Button handleClick={() => console.log('clicou')}>Clique em mim</Button>
       </div>
     )
   }
