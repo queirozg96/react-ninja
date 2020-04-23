0b531be0431e20d1d0aa1977b458d6b9c86528a5
Gabriel de Paula Queiroz <queirozg96@outlook.com>
2020-04-22 23:06:57 -0300

    M1#A34

---
+++ github-app/src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        My app
+      </div>
+    )
+  }
+}
+
+export default App
