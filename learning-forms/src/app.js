c2fa1cd31ce03bdc90e056cb1f520aff4a6f325a
Gabriel de Paula Queiroz <queirozg96@outlook.com>
2020-04-22 22:30:59 -0300

    M1#A31

--- learning-forms/src/app.js
+++ learning-forms/src/app.js
@@ -6,34 +6,22 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Valor inicial',
-      checked: false
+      value: '2'
     }
   }
   render () {
     return (
       <div>
         <form>
-          <input type='text' value={this.state.value} onChange={e => {
+          <select multiple value={['1', '2']} onChange={(e) => {
             this.setState({
               value: e.target.value
             })
-          }} />
-          <label>
-            <input
-              type='checkbox'
-              value='my-checkbox'
-              checked={this.state.checked}
-              onChange={
-                e => {
-                  this.setState({ checked: e.target.checked })
-                }
-              } />
-            Checkbox
-          </label>
-
-          <input type='radio' name='rd' value='1'/> Radio 1
-          <input type='radio' name='rd' value='2'/> Radio 2
+          }}>
+            <option value='1'>Opção 1</option>
+            <option value='2'>Opção 2</option>
+            <option value='3'>Opção 3</option>
+          </select>
         </form>
       </div>
     )
