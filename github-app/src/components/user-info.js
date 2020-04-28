4f1ec364155880d456221c6d7d3d3030d16f6230
Gabriel Queiroz <gabrielqueiroz@MBP-de-Gabriel.home>
2020-04-28 09:53:47 -0300

    M1#A36

---
+++ github-app/src/components/user-info.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars1.githubusercontent.com/u/28719964?v=4' />
+    <h1>
+      <a href='https://github.com/queirozg96'>Gabriel de Paula</a>
+    </h1>
+    <ul className='repos-info'>
+      <li> Repositórios: 2</li>
+      <li> Seguidores: 1</li>
+      <li> Seguindo: 1</li>
+    </ul>
+  </div>
+)
+
+export default UserInfo
