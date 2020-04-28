a20aaa94f7c3a0856b0cc542b008fa157e0acdd6
Gabriel Queiroz <gabrielqueiroz@MBP-de-Gabriel.home>
2020-04-28 11:38:33 -0300

    M1#A38

--- github-app/src/app.js
+++ github-app/src/app.js
@@ -7,12 +7,27 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-
+      userinfo: {
+        username: 'Gabriel de Paula Queiroz',
+        repos: 1,
+        followers: 1,
+        following: 1
+      },
+      repos: [{
+        name: 'Repo',
+        link: '#'
+      }],
+      starred: [{
+        name: 'Repo',
+        link: '#'
+      }]
     }
   }
 
   render () {
-    return <AppContent />
+    return <AppContent userinfo={this.state.userinfo}
+      repos={this.state.repos}
+      starred={this.state.starred} />
   }
 }
 
