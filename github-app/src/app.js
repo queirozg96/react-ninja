bdce504f91a567a6d43e4141802a9f6566a9fd66
Gabriel Queiroz <gabrielqueiroz@MBP-de-Gabriel.home>
2020-04-28 11:21:41 -0300

    M1#A37

--- github-app/src/app.js
+++ github-app/src/app.js
@@ -1,10 +1,7 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Search from './components/search'
-import UserInfo from './components/user-info'
-import Actions from './components/actions'
-import Repos from './components/repos'
+import AppContent from './components/app-content'
 
 class App extends Component {
   constructor () {
@@ -13,27 +10,9 @@ class App extends Component {
 
     }
   }
-  
-  render () {
-    return (
-      <div className='app'>
-        <Search />
-        <UserInfo />
-        <Actions />
-        <Repos className='repos' title='Repositórios'
-          repos={[{
-            name: 'Nome do repositório',
-            link: '#'
-          }]} />
 
-        <Repos className='starred'
-          title='Favoritos'
-          repos={[{
-            name: 'Nome do repositório',
-            link: '#'
-          }]} />
-      </div>
-    )
+  render () {
+    return <AppContent />
   }
 }
 
