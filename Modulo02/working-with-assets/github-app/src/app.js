3bf0b2adf61b90368a4438be2203bc29e900a6ad
Gabriel de Paula Queiroz <queirozg96@outlook.com>
2020-05-10 12:42:49 -0300

    M2#A31

---
+++ Modulo02/working-with-assets/github-app/src/app.js
@@ -0,0 +1,78 @@
+'use strict'
+
+import React, { Component } from 'react'
+import AppContent from './components/app-content'
+import ajax from '@fdaciuk/ajax'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      userinfo: null,
+      repos: [],
+      starred: [],
+      isFetching: false
+    }
+
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  getGithubUrl (username, type) {
+    const internalUser = username ? `/${username}` : ''
+    const internalType = type ? `/${type}` : ''
+    return `https://api.github.com/users${internalUser}${internalType}`
+  }
+
+  handleSearch (e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    if (keyCode === ENTER) {
+      this.setState({ isFetching: true })
+      ajax().get(this.getGithubUrl(value))
+      .then((result) => {
+        this.setState({
+          userinfo: {
+            username: result.name,
+            photo: result.avatar_url,
+            login: result.login,
+            repos: result.public_repos,
+            followers: result.followers,
+            following: result.following
+          },
+          repos: [],
+          starred: []
+        })
+      })
+      .always(() => this.setState({ isFetching: false }))
+    }
+  }
+
+  getRepos (type) {
+    return (e) => {
+      ajax().get(this.getGithubUrl(this.state.userinfo.login, type))
+      .then(result => {
+        this.setState({
+          [type]: result.map(repo => (
+            {
+              name: repo.name,
+              link: repo.html_url
+            }
+          ))
+        })
+      })
+    }
+  }
+
+  render () {
+    return <AppContent
+      {...this.state}
+      handleSearch={this.handleSearch}
+      getRepos={this.getRepos('repos')}
+      getStarred={this.getRepos('starred')}
+    />
+  }
+}
+
+export default App
