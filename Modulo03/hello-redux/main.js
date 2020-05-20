f2ccb455e553ec4a038cc4fd6d2cc18616e745e8
Gabriel Queiroz <queirozg96@outlook.com.br>
2020-05-19 22:56:45 -0300

    M3#A08

--- Modulo03/hello-redux/main.js
+++ Modulo03/hello-redux/main.js
@@ -1,11 +1,37 @@
 'use strict'
 
+const counter = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+  }
+
+  if (typeof state === 'undefined')
+    return 0;
+    
+  return state
+}
+
 console.assert(
-  1 === 2,
-  'Teste 1 = 2'
+  counter(0, { type: 'INCREMENT' }) === 1
 )
 
 console.assert(
-  2 === 2,
-  'Teste 2 = 2'
+  counter(1, { type: 'INCREMENT' }) === 2
+)
+
+console.assert(
+  counter(2, { type: 'DECREMENT' }) === 1
+)
+
+console.assert(
+  counter(5, { type: 'DECREMENT' }) === 4
+)
+
+console.assert(
+  counter(3, { type: 'SOMETHING' }) === 3
+)
+
+console.assert(
+  counter(undefined, {}) === 0
 )
