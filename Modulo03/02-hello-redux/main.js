1218fdefd2c38a29478cf52467cf4b8112b94e9b
Gabriel de Paula Queiroz <queirozg96@outlook.com>
2020-06-15 18:44:12 -0300

    M3#A09

---
+++ Modulo03/02-hello-redux/main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+  }
+
+  if (typeof state === 'undefined')
+    return 0
+    
+  return state
+}
+
+const { createStore } = Redux
+const store = createStore(counter)
+
+store.subscribe(() => {
+  console.log('disparou uma ação!', store.getState())
+})
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({type: 'DECREMENT'})
